02f5137f7923f3c83c8a542f68a16f53da60124d
Azim Momin <ayyem@protonmail.com>
2024-04-17 20:45:14 -0400

    Hooked up player movement

--- game/player.py
+++ game/player.py
@@ -2,23 +2,27 @@ import pygame
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos: pygame.Vector2, *groups) -> None:
+    def __init__(self, pos: tuple[int, int], *groups) -> None:
         super().__init__(*groups)
 
         self.image = pygame.Surface((50, 50))
         self.image.fill("red")
+
         self.rect = self.image.get_rect(center=pos)
+        self.position = pygame.math.Vector2(self.rect.center)
+
+        self.direction = pygame.math.Vector2()
+        self.speed: float = 200.0
 
     def update(self, dt: float):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
-            print("Up")
+        # We convert the boolean sotred in keys to an int (0 or 1) to
+        # determine the direction
+        self.direction.x = keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]
+        self.direction.y = keys[pygame.K_DOWN] - keys[pygame.K_UP]
 
-        if keys[pygame.K_DOWN]:
-            print("Down")
+        if self.direction.length_squared() > 0:
+            self.direction.normalize_ip()
 
-        if keys[pygame.K_LEFT]:
-            print("Left")
-
-        if keys[pygame.K_RIGHT]:
-            print("Right")
+        self.position += self.direction * self.speed * dt
+        self.rect.center = (round(self.position.x), round(self.position.y))
